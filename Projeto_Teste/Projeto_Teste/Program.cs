aa387b1c40d446561f13bbb3d8ac0fcf393ab785
Luiz Roberto <110065527+LuizRobert33@users.noreply.github.com>
2024-11-06 23:18:33 -0300

    Update

--- Projeto_Teste/Projeto_Teste/Program.cs
+++ Projeto_Teste/Projeto_Teste/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using Accord.Neuro;
 using Accord.Neuro.Learning;
-using Accord.Math;
 
 class Program
 {
@@ -15,7 +14,6 @@ class Program
             new double[] { 599.23, 150.56, 91.84 },
             new double[] { 511.37, 153.02, 93.95 },
             new double[] { 789.87, 152.79, 94.33 }
-            // Adiconar mais dados caso seja necessario
         };
 
         double[][] outputs = {
@@ -25,17 +23,27 @@ class Program
             new double[] { 93.95 },
             new double[] { 94.33 },
             new double[] { 94.08 }
-            //  Adiconar mais dados caso seja necessario
         };
 
-        // Criação da rede neural com 3 entradas, uma camada oculta de 5 neurônios e 1 neurônio de saída
+        // Normalização dos dados de entrada e saída
+        NormalizeData(ref inputs);
+        NormalizeData(ref outputs);
+
+        // Verificação dos tamanhos das entradas e saídas
+        if (inputs.Length != outputs.Length)
+        {
+            Console.WriteLine("Erro: O número de amostras de entrada não corresponde ao número de saídas.");
+            return;
+        }
+
+        // Criação da rede neural com 3 entradas, uma camada oculta de 10 neurônios e 1 neurônio de saída
         var network = new ActivationNetwork(
             function: new SigmoidFunction(),
-            inputsCount: 3,  // 3 entradas: vazão e nível do reservatório
-            neuronsCount: 5  // 5 neurônios na camada oculta
+            inputsCount: 3,  // 3 entradas
+            neuronsCount: new int[] { 10, 1 } // 10 neurônios na camada oculta, 1 na camada de saída
         );
 
-        // Inicialização dos pesos da rede com o algoritmo NguyenWidrow
+        // Inicialização dos pesos da rede com o algoritmo Nguyen-Widrow
         new NguyenWidrow(network).Randomize();
 
         // Configuração do algoritmo de aprendizado
@@ -61,10 +69,66 @@ class Program
         // Dados de entrada para teste
         double[] testInput = { 789.87, 150.56, 91.84 };
 
+        // Normaliza os dados de entrada para teste
+        NormalizeTestData(ref testInput);
+
         // Fazendo uma previsão com a rede treinada
         double[] result = network.Compute(testInput);
 
         // Exibindo o resultado
         Console.WriteLine("Previsão do nível do reservatório Santa Rosa: " + result[0]);
+    }
+
+    // Função de normalização de dados (normaliza para o intervalo 0-1)
+    static void NormalizeData(ref double[][] data)
+    {
+        for (int i = 0; i < data.Length; i++)
+        {
+            double max = FindMax(data[i]);
+            double min = FindMin(data[i]);
+            for (int j = 0; j < data[i].Length; j++)
+            {
+                data[i][j] = (data[i][j] - min) / (max - min);
+            }
+        }
+    }
+
+    // Função de normalização dos dados de entrada para teste
+    static void NormalizeTestData(ref double[] data)
+    {
+        double max = FindMax(data);
+        double min = FindMin(data);
+        for (int i = 0; i < data.Length; i++)
+        {
+            data[i] = (data[i] - min) / (max - min);
+        }
+    }
+
+    // Função auxiliar para encontrar o valor máximo de um vetor
+    static double FindMax(double[] data)
+    {
+        double max = data[0];
+        foreach (double val in data)
+        {
+            if (val > max)
+            {
+                max = val;
+            }
+        }
+        return max;
+    }
+
+    // Função auxiliar para encontrar o valor mínimo de um vetor
+    static double FindMin(double[] data)
+    {
+        double min = data[0];
+        foreach (double val in data)
+        {
+            if (val < min)
+            {
+                min = val;
+            }
+        }
+        return min;
     }
 }
